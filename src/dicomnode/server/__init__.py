f5782171ab05fb11df03823e728af21d77ffb2cb
Christoffer Vilstrup Jensen <demiguard@gmail.com>
2024-02-14 15:22:44 +0100

    Server module could not load before

--- src/dicomnode/server/__init__.py
+++ src/dicomnode/server/__init__.py
@@ -1,4 +1,6 @@
 """Modules related to creating a data processing pipeline or a dicomnode"""
+from .. import lib as __lib # This is ensure correct loading
+
 from . import factories
 from . import grinders
 from . import input
@@ -6,3 +8,16 @@ from . import maintenance
 from . import nodes
 from . import output
 from . import pipeline_tree
+
+__all__ = (
+  'factories',
+  'grinders',
+  'input',
+  'maintenance',
+  'nodes',
+  'output',
+  'pipeline_tree',
+)
+
+def __dir__():
+  return __all__
